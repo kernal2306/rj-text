3868189f0624885526f8c5cca2f31ebc2cdcf14d
kernal2306 <kernal2306@hotmail.com>
2016-02-04 10:54:22 -0800

    adding first couple classes

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main main = new Main();
+		main.Start();
+
+	}
+	
+	public void Start(){
+		Window display = new Window();
+		
+	}
+
+}
