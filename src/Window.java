ac13bf408c0077e158ef4c4f9492fb64b71d0982
kernal2306 <kernal2306@hotmail.com>
2016-02-04 12:32:35 -0800

    changed layout to absolute positioning

--- src/Window.java
+++ src/Window.java
@@ -9,9 +9,23 @@ public class Window {
 		JFrame frame = new JFrame("Realm Jumper");
 		frame.setVisible(true);
 		frame.setSize(800, 600);
+		frame.setLayout(null);
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel emptyLabel = new JLabel("");
-		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		//JLabel emptyLabel = new JLabel("");
+		frame.getContentPane();
+		
+		JLabel textArea = new JLabel("Testing to see");
+		//textArea.setSize(700, 500);
+		textArea.setBounds(0,0,800,500);
+		textArea.setVisible(true);
+		frame.add(textArea);
+		JButton buttonLeft = new JButton("Left");
+		//buttonLeft.setSize(100,100);
+		buttonLeft.setBounds(200,500,100,40);
+		frame.add(buttonLeft);
+		JButton buttonRight = new JButton("Right");
+		buttonRight.setBounds(500, 500, 100, 40);
+		frame.add(buttonRight);
 	}
 	
 }
